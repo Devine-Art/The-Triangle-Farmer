89a056786201dd06e1adfcf0832e33445c6b6c6d
Devine-Art <adevine149@gmail.com>
2025-02-04 17:12:48 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Formats.Asn1;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Triangle Farmer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for variables and assign them
+            Console.WriteLine("Enter base of triangle:");
+            int basenumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter height of triangle:");
+            int heightnumber = int.Parse(Console.ReadLine());
+
+
+            //Calculate area
+            double areasize = (basenumber * heightnumber) *.5
+
+            //write area to console
+	    Console.WriteLine("area of triangle");
+            Console.WriteLine(areasize);
+
+        }
+    }
+}
